d3ecb3147debaff17c51f7f6b4c3cccd89647275
Fadhlullah Mustapha <fadhmusty@gmail.com>
2025-12-01 15:02:02 +0100

    Remove node_modules and .next from git tracking

--- .next/dev/static/chunks/app_login_page_tsx_c0ac49f8._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules__pnpm_b3b44886._.js",
-  "static/chunks/_f29afa83._.js"
-],
-    source: "dynamic"
-});
--- .next/dev/static/chunks/app_page_tsx_6bece898._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_9be3d5ac._.js"
-],
-    source: "dynamic"
-});
